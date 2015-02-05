609c26000537c914106ab07a371d0ed1e080b366
Aniket Lawande <anlawande@gmail.com>
2015-02-05 13:20:23 -0500

    Changes to bootup script

--- script.sh
+++ script.sh
@@ -1,5 +1,7 @@
 sudo apt-get update
 sudo apt-get install -y default-jre
-sudo apt-get install -y wget
+sudo apt-get install -y wget git curl
+curl -sL https://deb.nodesource.com/setup | sudo bash -
+sudo apt-get install nodejs
 wget http://mirrors.jenkins-ci.org/war/latest/jenkins.war
 java -jar jenkins.war --httpPort=8080
