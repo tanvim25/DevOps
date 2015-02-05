14f7161d19fc350e22035e00fc2fed30013eb963
Tanvi Mainkar <tmainka@ncsu.edu>
2015-02-05 13:15:52 -0500

    grunt

---
+++ Gruntfile.js
@@ -0,0 +1,31 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+
+	  less: 
+	  {
+	      development: 
+	      {
+	        options: 
+	        {
+	          compress: true,
+	          yuicompress: true,
+	          optimization: 2
+	        },
+	        files: 
+	        [{
+	  				expand: true,
+	  				cwd: "public",
+	  				src: "**/main.less",
+	  				dest: "public",
+	  				ext: ".css"
+	        }]
+	      }
+	  }
+
+  });
+
+  // This will automatically load any grunt plugin you install, such as grunt-contrib-less.
+  //require('load-grunt-tasks')(grunt);
+	grunt.loadNpmTasks('grunt-contrib-less');
+};
