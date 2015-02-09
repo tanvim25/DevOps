dfccd94952f7d8b2cec481864ba487ed3fe7dba2
Aniket Lawande <anlawande@gmail.com>
2015-02-08 20:54:43 -0500

    Changed server url

--- server.js
+++ server.js
@@ -1,7 +1,4 @@
 var express = require('express');
 var app = express();
-//app.configure(function () {
-    app.use('/test', express.static(__dirname+'/public') //where your static content is located in your filesystem
-    );
-//});
+app.use('/', express.static(__dirname+'/public'));
 app.listen(3000);
