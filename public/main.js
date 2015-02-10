a07f9f80b7aa5300ba194adc2fc743d7a26dc7b8
Aniket Lawande <anlawande@gmail.com>
2015-02-09 21:08:52 -0500

    added omnibox

--- public/main.js
+++ public/main.js
@@ -1,8 +1,22 @@
-var app = angular.module("app", ['ngRoute']);
+var app = angular.module("app", []);
 
-app.config(['$routeProvider', function($routeProvider) {
-	$routeProvider.when('/', {
-		templateUrl: 'partials/main.html',
-	})
-	.otherwise('/');
+app.controller("autoCompleteCtrl", ["$scope", function($scope){
+	$scope.results = [];
+	$scope.trie = new Trie();
+	$scope.trie.add("Something");
+	$scope.trie.add("Nothing");
+	$scope.trie.add("Anything");
+	$scope.trie.add("The quick brown fox jumped over the lazy dog");
+	$scope.trie.add("Lord of the Rings");
+	$scope.trie.add("Harry Potter");
+	$scope.trie.add("DevOps");
+	$scope.trie.add("Google");
+	$scope.trie.add("Amazon");
+	$scope.trie.add("Sinatra");
+	$scope.trie.add("Trance");
+	$scope.trie.add("Some more text");
+	
+	$scope.$watch('text', function(newVal) {
+		$scope.results = $scope.trie.lookup(newVal);
+	});
 }]);
