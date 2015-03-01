630a98be7acef0b1871337a9761c628ec39e9822
Aniket Lawande <anlawande@gmail.com>
2015-02-28 22:00:00 -0500

    Working simple controller test

--- test/autocomplete.js
+++ test/autocomplete.js
@@ -1,5 +1,17 @@
 describe("should test autocomplete feature", function() {
-	it("shoudld be true", function() {
-		expect(true).toEqual(true);
+	var scope;
+	
+	beforeEach(function() {
+		module('app');
+	});
+	
+	beforeEach(inject(function(_$controller_, $rootScope){
+        $controller = _$controller_;
+		scope = $rootScope.$new();
+	}));
+	
+	it("should be true", function() {
+		$controller('autoCompleteCtrl', { $scope: scope});
+		expect(scope.trie.count).toEqual(12);
 	});
 });
