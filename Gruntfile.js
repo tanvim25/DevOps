54ada2b9d599d6f1b61c011cf69b7de1f1f53d81
Aniket Lawande <anlawande@gmail.com>
2015-03-06 20:23:02 -0500

    istanbul coverage task

--- Gruntfile.js
+++ Gruntfile.js
@@ -37,16 +37,35 @@ module.exports = function(grunt) {
 
 		jasmine : {
 			// Your project's source files
-			src : ['public/main.js', 'public/MyTrie/trie-browser.js'],
-			options: {
-				vendor: ['public/lib/angular.min.js', 'test/jasmine-2.1.3/angular-mocks.js'],
-				// Your Jasmine spec files
-				specs : 'test/autocomplete.js',
+			all : {
+				src : ['public/main.js'],
+				options: {
+					vendor: ['public/lib/angular.min.js', 'test/jasmine-2.1.3/angular-mocks.js', 'public/MyTrie/trie-browser.js'],
+					// Your Jasmine spec files
+					specs : 'test/autocomplete.js',
+				},
+			},
+
+			istanbul: {
+				src: '<%= jasmine.all.src %>',
+				options: {
+					vendor: '<%= jasmine.all.options.vendor %>',
+					specs: '<%= jasmine.all.options.specs %>',
+					template: require('grunt-template-jasmine-istanbul'),
+					templateOptions: {
+						coverage: 'coverage/json/coverage.json',
+						report: [
+							{type: 'html', options: {dir: 'coverage/html'}},
+							{type: 'text-summary'}
+						]
+					}
+				}
 			}
-		}
+		},
+
 
 	});
-	
+
 	grunt.registerTask("initHooks", function() {
 		grunt.file.copy("./gitHooks/pre-commit", "./.git/hooks/pre-commit");
 	});
@@ -59,4 +78,5 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-jasmine');
 	grunt.registerTask('build_all', ['clean','less','uglify']);
 	grunt.registerTask("default", ["build_all"]);
+	grunt.registerTask("istanbul", "istanbul");
 };
