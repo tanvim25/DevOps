c3922b15b14ea47414d52437a43daedcb307e55c
tanvim25 <tanvi.mainkar@gmail.com>
2015-03-09 20:05:41 -0400

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -24,7 +24,7 @@ module.exports = function(grunt) {
 		},
 
 		clean: {
-			files: ["public/main.css", "public/main.min.js"],
+			files: ["public/main.css", "public/main.min.js", "pmd.xml"],
 		},
 
 		uglify:{
